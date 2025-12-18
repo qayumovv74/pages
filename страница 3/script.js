9b9308e4628916f9ca8aec2a50ded09e725e2c96
qayumovv74 <k.abdu7744@gmail.com>
2025-12-19 04:59:30 +0500

    Delete страница 3 directory

--- страница 3/script.js
+++
@@ -1,8 +0,0 @@
-function showBookingForm() {
-    document.getElementById('booking-form').classList.toggle('hidden');
-}
-
-function bookTour() {
-    document.getElementById('booking-form').classList.add('hidden');
-    document.getElementById('booking-confirmation').classList.remove('hidden');
-}
